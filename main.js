3a0aadc17ad3236a0ea527b5aae4857df864bd9d
Vinicius Silva <slvinicius00@gmail.com>
2022-11-03 07:31:56 -0300

    23/11

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ document.querySelector('#app').innerHTML = `
                 ${createCard("20/11", "domingo", createGame("qatar", "13:00", "ecuador"))}
                 ${createCard("21/11", "segunda", createGame("england", "10:00", "iran") + createGame("senegal", "13:00", "netherlands") + createGame("usa", "16:00", "wales"))}
                 ${createCard("22/11", "terça", createGame("argentina", "07:00", "arabia") + createGame("denmark", "10:00", "tunisia") + createGame("mexico", "13:00", "poland") + createGame("france", "16:00", "australia"))}
-                ${createCard("23/11", "quarta", createGame())}
+                ${createCard("23/11", "quarta", createGame("morocco", "07:00", "croatia") + createGame("germany", "10:00", "japan") + createGame("spain", "13:00", "costarica") + createGame("belgium", "16:00", "canada"))}
                 ${createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia"))}
                 ${createCard("25/11", "sexta", createGame())}
                 ${createCard("26/11", "sábado", createGame())}
